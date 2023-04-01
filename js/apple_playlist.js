5e1169efe9e2fe25566d2c9212c4d6c2cec3bf19
Shubham Rawat <sr856161@gmail.com>
2023-04-01 12:27:02 +0530

    fix playlist scrapping

--- js/apple_playlist.js
+++ js/apple_playlist.js
@@ -30,27 +30,36 @@ module.exports.getPlaylist = async (playlistUrl) => {
     const soup = new JSSoup(data);
 
     //scraping...
-    const playlistHeaderBlock = soup.find("div", "album-header-metadata");
-    let playlistName = playlistHeaderBlock.find("h1").text.trim();
-    let playlistUser = playlistHeaderBlock
-      .find("div", "product-creator")
-      .text.trim();
-    //   console.log(playlistName, playlistUser);
+    const playlistHeaderBlock = soup.find("div", "container-detail-header");
+    let playlistName, playlistUser;
+    try {
+      playlistName = playlistHeaderBlock.find("h1").text.trim();
+      playlistUser = playlistHeaderBlock
+        .find("p", "headings__subtitles")
+        .text.trim();
+    } catch (err) {
+      playlistName = "";
+      playlistUser = "";
+    }
+
+    // console.log(playlistName, playlistUser);
     playlistObj.playlist = htmlEntities.decode(playlistName);
     playlistObj.user = htmlEntities.decode(playlistUser);
 
-    const tracksInfo = soup.findAll("div", "songs-list-row");
-    // console.log(tracksInfo.length);
+    const tracksInfo = soup.findAll("div", "songs-list-row"); //finding all songs info
     playlistObj.songs = [];
 
     for (let track of tracksInfo) {
-      let songName = track.find("div", "songs-list-row__song-name").text;
-      let singerNames = track.find("div", "songs-list-row__by-line").text;
+      let songName = track.find("div", "songs-list__col--song").text;
+      console.log(songName);
+      let singerNames = track.find("div", "songs-list__col--secondary").text;
+      let album = track.find("div", "songs-list__col--tertiary").text;
       singerNames = singerNames.replace(/\s{2,10}/g, ""); //remove spaces
       songName = songName.replace(/\?|<|>|\*|"|:|\||\/|\\/g, ""); //removing special characters which are not allowed in file name
       playlistObj.songs.push({
         name: htmlEntities.decode(songName),
         singer: htmlEntities.decode(singerNames),
+        album: htmlEntities.decode(album),
       });
     }
     playlistObj.total = playlistObj.songs.length;
