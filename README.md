f958dccca739ce905270d92b1c4f9a140b4db4a6
Shubham Rawat <Sr856161@gmail.com>
2023-08-31 00:27:31 +0530

    fix get playlist

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Web app to download your favourite apple music playlist to mp3 directly!
 
 > Open: https://apple-playlist-downloader-pvx.onrender.com/
 
-> Paste your playlist link like:<br> https://music.apple.com/fi/playlist/one-direction-essentials/pl.134ef3b46d32414e9b4b5a995a2f3ea7
+> Paste your playlist link like: `https://music.apple.com/fi/playlist/one-direction-essentials/pl.134ef3b46d32414e9b4b5a995a2f3ea7`
 
 > Get all the songs direct download link to download from a single page!
 
--- js/apple_playlist.js
+++ js/apple_playlist.js
@@ -2,68 +2,27 @@ const axios = require("axios");
 const JSSoup = require("jssoup").default;
 const htmlEntities = require("html-entities");
 
-module.exports.getPlaylist = async (playlistUrl) => {
+module.exports.getPlaylist = async (url) => {
   console.log("REQUEST TO FIND PLAYLIST INFO!");
-  // return {
-  //   total: 2,
-  //   playlist: "FAVOURITE",
-  //   user: "Shubham",
-  //   songs: [
-  //     {
-  //       name: "love song",
-  //       singer: "why dont we",
-  //     },
-  //     {
-  //       name: "Story Of My Life",
-  //       singer: "One Direction",
-  //     },
-  //     {
-  //       name: "lucid dreams",
-  //       singer: "juice wrld",
-  //     },
-  //   ],
-  // };
   let playlistObj = {};
   try {
-    const response = await axios.get(playlistUrl);
-    const { data } = response;
-    const soup = new JSSoup(data);
+    let api = "https://api.fabdl.com/apple-music/get?url=";
 
-    //scraping...
-    const playlistHeaderBlock = soup.find("div", "container-detail-header");
-    let playlistName, playlistUser;
-    try {
-      playlistName = playlistHeaderBlock.find("h1").text.trim();
-      playlistUser = playlistHeaderBlock
-        .find("p", "headings__subtitles")
-        .text.trim();
-    } catch (err) {
-      playlistName = "";
-      playlistUser = "";
-    }
+    console.log("Playlist URL: ", url);
+    const { data } = await axios.get(api + url);
 
-    // console.log(playlistName, playlistUser);
-    playlistObj.playlist = htmlEntities.decode(playlistName);
-    playlistObj.user = htmlEntities.decode(playlistUser);
-
-    const tracksInfo = soup.findAll("div", "songs-list-row"); //finding all songs info
+    playlistObj.playlist = htmlEntities.decode(data.result.name);
+    playlistObj.user = htmlEntities.decode(data.result.owner);
     playlistObj.songs = [];
 
-    for (let track of tracksInfo) {
-      let songName = track.find("div", "songs-list__col--song").text;
-      console.log(songName);
-      let singerNames = track.find("div", "songs-list__col--secondary").text;
-      let album = track.find("div", "songs-list__col--tertiary").text;
-      singerNames = singerNames.replace(/\s{2,10}/g, ""); //remove spaces
-      songName = songName.replace(/\?|<|>|\*|"|:|\||\/|\\/g, ""); //removing special characters which are not allowed in file name
+    data.result.tracks.forEach((track) => {
       playlistObj.songs.push({
-        name: htmlEntities.decode(songName),
-        singer: htmlEntities.decode(singerNames),
-        album: htmlEntities.decode(album),
+        name: htmlEntities.decode(track.name),
+        singer: htmlEntities.decode(track.artists),
       });
-    }
-    playlistObj.total = playlistObj.songs.length;
-    // console.log(playlistObj);
+    });
+
+    return playlistObj;
   } catch {
     console.log("SOME ERROR WITH PLAYLIST URL!");
     playlistObj = null;
