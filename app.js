5ff8562d034a631924b173bd64e15ddaea6d81d7
Alish-Rawat <rawatalish4@gmail.com>
2024-09-20 23:46:13 +0530

    added YoutubeDl for Downloading the Songs

--- app.js
+++ app.js
@@ -1,49 +1,80 @@
 const express = require("express");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const axios = require("axios");
+// const axios = require("axios");
+const ytdl = require("ytdl-core");
+const yts = require("yt-search");
+const youtubedl = require("youtube-dl-exec");
+const fs = require("fs");
+// var bodyParser = require("body-parser");
+// var fs = require("fs");
+var path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const INFO_URL = "https://slider.kz/vk_auth.php?q=";
-const DOWNLOAD_URL = "https://slider.kz/download/";
+// const INFO_URL = "https://slider.kz/vk_auth.php?q=";
+// const DOWNLOAD_URL = "https://slider.kz/download/";
 
 const getSong = async (song, singer) => {
-  let query = (song + "%20" + singer).replace(/\s/g, "%20");
-  console.log(INFO_URL + query);
-  const { data } = await axios.get(encodeURI(INFO_URL + query));
+  // let query = (song + "%20" + singer).replace(/\s/g, "%20");
+  // console.log(INFO_URL + query);
+  // const { data } = await axios.get(encodeURI(INFO_URL + query));
+
+  //----------------------------------------------------//
+  // const data = [];
+  const r = await yts(`${song} +" - "+${singer}`);
+
+  const videoID = r.all[0].url;
+  console.log(videoID);
+
+  let info = await ytdl.getInfo(videoID);
+
+  let songDownloadUrl = videoID;
+  let songTitleFound = info.videoDetails.title;
 
   // when no result then [{}] is returned so length is always 1, when 1 result then [{id:"",etc:""}]
-  if (!data["audios"][""][0].id) {
+  if (!songDownloadUrl) {
     return { matchTrack: "NOT FOUND!", url: "" };
   } else {
     //avoid remix,revisited,mix
-    let i = 0;
-    let track = data["audios"][""][i];
-    let totalTracks = data["audios"][""].length;
-    while (
-      i < totalTracks &&
-      /remix|revisited|reverb|mix/i.test(track.tit_art)
-    ) {
-      i += 1;
-      track = data["audios"][""][i];
-    }
-    //if reach the end then select the first song
-    if (!track) {
-      track = data["audios"][""][0];
-    }
+    // let i = 0;
+    // let track = data["audios"][""][i];
+    // let totalTracks = data["audios"][""].length;
+    // while (
+    //   i < totalTracks &&
+    //   /remix|revisited|reverb|mix/i.test(track.tit_art)
+    // ) {
+    //   i += 1;
+    //   track = data["audios"][""][i];
+    // }
+    // //if reach the end then select the first song
+    // if (!track) {
+    //   track = data["audios"][""][0];
+    // }
 
     // let link = DOWNLOAD_URL + track.id + "/";
     // link = link + track.duration + "/";
     // link = link + track.url + "/";
     // link = link + track.tit_art + ".mp3" + "?extra=";
+
     // link = link + track.extra;
-    let link = track.url;
-    link = encodeURI(link);
-    return { matchTrack: track.tit_art, url: link };
+    // await youtubedl(videoID, {
+    //   format: "m4a",
+    //   output: "./songs/" + info.videoDetails.title + ".mp3",
+    //   maxFilesize: "104857600",
+    //   preferFreeFormats: true,
+    // });
+    let link = songDownloadUrl;
+    // let link =
+    //   "https://static.toiimg.com/thumb/msid-104375923,width-1280,height-720,imgsize-29630,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg";
+    // link = encodeURIComponent(link);
+    return {
+      matchTrack: info.videoDetails.title,
+      urlLink: link,
+    };
   }
 };
 
@@ -99,11 +130,95 @@ app.post("/find", (req, res) => {
   let song = req.body.song;
 
   getSong(song, singer).then((response) => {
-    console.log("SENDING RESPONSE:" + response);
+    // console.log("SENDING RESPONSE:" + response);
     res.send(response);
   });
 });
 
-app.listen(process.env.PORT || 80, () => {
+// app.use(bodyParser.raw({ type: "application/octet-stream", limit: "2mb" }));
+
+// app.post("/download", function (req, res) {
+//   var filePath = path.join(__dirname, "./a.mp3");
+//   fs.open(filePath, "w", function (err, fd) {
+//     fs.write(fd, req.body, 0, req.body.length, null, function (err) {
+//       if (err) throw "error writing file: " + err;
+//       fs.close(fd, function () {
+//         console.log("wrote the file successfully");
+//         res.status(200).end();
+//       });
+//     });
+//   });
+//   console.log("downlaod Api hit");
+// });
+
+app.get("/download", async (req, res) => {
+  // files comes as string in object //
+  const fileNameObj = req.query.filename;
+  const k = JSON.parse(fileNameObj);
+  // let fileName = { namee: "matchedSong.matchTrack", url: "url" };
+
+  // const url = req.query.url;
+  console.log("fileName -" + k.namee);
+  console.log("URL -" + k.url);
+
+  await youtubedl(k.url, {
+    format: "m4a",
+    output: "./songs/" + k.namee + ".mp3",
+    maxFilesize: "104857600",
+    preferFreeFormats: true,
+  });
+  // let song = req.body.songName;
+  // console.log(song);
+
+  const audioPath = path.join(__dirname, `/songs/${k.namee}.mp3`); // Replace with your audio file path
+  // console.log(JSON.stringify(audioPath));
+
+  fs.readFile(audioPath, (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading audio file");
+      return;
+    }
+
+    // Set the content type to audio/mpeg (or another appropriate audio type)
+    res.setHeader("Content-Type", "audio/mpeg");
+    res.setHeader("Content-Disposition", `attachment; filename="${k.namee}" `);
+
+    // Send the audio buffer
+    res.send(data);
+    // console.log("Download APi hit");
+  });
+  fs.unlink(audioPath, (err) => {
+    if (err) {
+      console.error("Error deleting the file:", err);
+      return;
+    }
+    console.log("File successfully deleted after download!");
+  });
+});
+
+// app.get("/download", (req, res) => {
+//   let song = req.body.songName;
+//   // // res.download(`./songs/+${k}+.mp3`);
+//   // for (let i = 0; i < songNameArray.length; ++i) {
+//   //   document
+//   //     .querySelector("#download-btn-" + songNumb)
+//   //     .addEventListener("click", () => {
+//   //       res.download(`./songs/One Direction - Best Song Ever.mp3`);
+//   //     });
+//   // }
+//   console.log("download API hit " + song);
+
+//   res.download("./songs/One Direction - Best Song Ever.mp3");
+//   // var file = fs.readFileSync(
+//   //   __dirname + "/songs/One Direction - Best Song Ever.mp3",
+//   //   "binary"
+//   // );
+
+//   // res.setHeader("Content-Length", file.length);
+//   // res.write(file, "binary");
+//   // res.end();
+// });
+
+app.listen(process.env.PORT || 8000, () => {
   console.log("SERVER STARTED!");
 });
--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,12 @@ for (singer of document.querySelectorAll(".singer-name")) {
 const updateMatchedSong = (songNumb, matchedSong) => {
   document.querySelector("#matched-" + songNumb).textContent =
     matchedSong.matchTrack;
+  let url = matchedSong.urlLink;
+  let namee = { namee: matchedSong.matchTrack, url: url };
+  console.log(url);
+  let filename = JSON.stringify(namee);
+  let k = document.querySelector("#download-btn-" + songNumb);
+  // console.log(filename);
 
   if (matchedSong.matchTrack !== "NOT FOUND!") {
     document
@@ -21,10 +27,43 @@ const updateMatchedSong = (songNumb, matchedSong) => {
       .classList.add("show-download"); //show download btn
     document
       .querySelector("#download-" + songNumb)
-      .setAttribute("href", matchedSong.url); //add url
-  } else {
-    document.querySelector("#download-" + songNumb).removeAttribute("href");
+      .addEventListener("click", (e) => {
+        k.innerHTML = `  <img
+                  type="gif"
+                  src="../images/downloading.gif"
+                  alt="gif"
+                  style="width:86px"
+                />`;
+        //           `<div style="position: relative; background-color: ; width: 7rem; height: 2rem;>
+        //     <h6 style="color: azure; font-size: 200px; font-weight: 200;">Downloading...</h6>
+        // </div>`
+        e.preventDefault();
+        fetch(`/download?filename=${filename}`)
+          .then((response) => response.arrayBuffer())
+          .then((buffer) => {
+            // Create a blob from the buffer
+            const audioBlob = new Blob([buffer], { type: "audio/mpeg" });
+
+            // Create a link element
+            const link = document.createElement("a");
+
+            // Create an object URL for the blob
+            const url = URL.createObjectURL(audioBlob);
+
+            // Set link's href to point to the object URL
+            link.href = url;
+            link.download = `${matchedSong.matchTrack}.mp3`; // Set the filename
+
+            // Programmatically click the link to trigger the download
+            link.click();
+
+            console.log(matchedSong.matchTrack);
+          })
+          .catch((error) => console.error("Error fetching audio:", error));
+      });
   }
+
+  ////////////////////////////////////////////////////
 };
 
 for (let i = 0; i < songNameArray.length; ++i) {
